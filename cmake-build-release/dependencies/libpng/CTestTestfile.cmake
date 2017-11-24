cd35706ab077f7171e065ed38d568a7d3f066a31
Joao Neves <joaoneves792@gmail.com>
2017-11-24 20:24:56 +0000

    libpng fix

--- cmake-build-release/dependencies/libpng/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/joao/workspace/CGJDemo/dependencies/libpng
-# Build directory: /home/joao/workspace/CGJDemo/cmake-build-release/dependencies/libpng
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(pngtest "./pngtest" "/home/joao/workspace/CGJDemo/dependencies/libpng/pngtest.png")
-add_test(pngvalid "./pngvalid")
