7cd67f97d7151bf699a7b4c59d1540d09b578fc7
Joao Neves <joaoneves792@gmail.com>
2017-11-26 15:36:38 +0000

    Replaced the Charger with the F430GTC

--- Demo/meshes.cpp
+++ Demo/meshes.cpp
@@ -7,7 +7,7 @@
 #include "Constants.h"
 
 void loadMeshes(){
-    ResourceManager::Factory::createMesh(CAR, "res/charger/charger.h3d");
+    ResourceManager::Factory::createMesh(CAR, "res/F430GTC/F430GTC.h3d");
     ResourceManager::Factory::createMesh(ROAD, "res/Road/road.h3d");
     ResourceManager::Factory::createMesh(SKY, "res/Road/sky.h3d");
 }
