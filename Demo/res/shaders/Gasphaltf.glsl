5599928ccf2af2661f1d7b838fab00cdd5c4f5cf
Joao Miguel Neves <joaoneves792@gmail.com>
2017-12-17 19:00:04 +0000

    First steps towards deferred shading

---
+++ Demo/res/shaders/Gasphaltf.glsl
@@ -0,0 +1,47 @@
+#version 330 core
+
+#define DIFFUSE 0
+#define MATERIAL 1
+#define NORMAL 2
+
+in vec3 eyeDirection_worldspace;
+in vec2 texture_coord_from_vshader;
+in vec3 normal_cameraspace;
+in vec3 normal_worldspace;
+
+out vec4[3] G_output;
+
+/*Material Properties*/
+uniform sampler2D texture_sampler;
+uniform samplerCube environment;
+uniform vec4 ambient;
+uniform vec4 diffuse;
+uniform vec4 specular;
+uniform vec4 emissive;
+uniform float shininess;
+uniform float transparency;
+
+uniform float movement; // get movement for the environment map
+
+void main() {
+	//Material properties
+	vec3 matDiffuse = (texture(texture_sampler, texture_coord_from_vshader).rgb * diffuse.xyz);
+
+    float alpha = texture(texture_sampler, texture_coord_from_vshader).a - transparency;
+
+    if(shininess >= 65.0f){
+	    vec3 N = normalize(normal_worldspace);
+        vec3 E = normalize(eyeDirection_worldspace);
+        vec3 R = reflect(E, N);
+        R = R + vec3(0.0f, 0.0f, movement);
+        matDiffuse += texture(environment, R).rgb*(shininess/128.0f)*0.1f*(1/alpha);
+    }
+
+    G_output[DIFFUSE].rgb = matDiffuse;
+    G_output[DIFFUSE].a = alpha;
+
+    G_output[MATERIAL].xyz = specular.rgb;
+    G_output[MATERIAL].w = shininess;
+
+    G_output[NORMAL].xyz = normal_cameraspace;
+}
